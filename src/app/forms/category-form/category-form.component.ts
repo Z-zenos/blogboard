09776fa9934f637056a67f0675722ede028f78b9
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2023-05-01 20:03:37 +0700

    [Update]: file upload

--- src/app/forms/category-form/category-form.component.ts
+++ src/app/forms/category-form/category-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./category-form.component.scss']
 })
 export class CategoryFormComponent implements OnInit {
+  @ViewChild('inputFileImg', { static: false }) inputFileImg!: ElementRef;
+  @ViewChild('btnFileImg', { static: false }) btnFileImg!: ElementRef;
+  @ViewChild('dragArea', { static: false }) dropArea!: ElementRef<HTMLDivElement>;
+  @ViewChild('dragText', { static: false }) dragText!: ElementRef;
+
+  file!: File;
   form!: FormGroup;
 
   @Output() closeFormEvent = new EventEmitter<boolean>();
@@ -31,4 +37,70 @@ export class CategoryFormComponent implements OnInit {
     this.closeFormEvent.emit(false);
   }
 
+  browseFileImg() {
+    this.inputFileImg.nativeElement.click();
+  }
+
+  onChange(e: Event) {
+    // Getting user select file and [0] this means if user select multiple files then we'll select only the first one
+    // @ts-ignore
+    this.file = (e.target as HTMLInputElement).files[0];
+    console.log(this.file);
+    this.dropArea.nativeElement.classList.add('active');
+    this.showFile();
+  }
+
+  // If user drag file over drop area
+  onDragOver(e: Event) {
+    e.preventDefault();
+    this.dropArea.nativeElement.classList.add('active');
+    this.dragText.nativeElement.textContent = 'Release';
+  }
+
+  // If user leave dragged file from droparea
+  onDragLeave() {
+    this.dropArea.nativeElement.classList.remove("active");
+    this.dragText.nativeElement.textContent = "Drag & Drop";
+  }
+
+  // If user drop file on drop area
+  onDrop(e: Event) {
+    e.preventDefault();
+
+    // Getting user select file and [0] means if user select multiple files then we'll  select only the first one.
+    // @ts-ignore
+    this.file = e.dataTransfer.files[0];
+
+    this.showFile();
+  }
+
+
+  showFile() {
+    // Getting selected file type
+    let fileType = this.file.type;
+
+    // adding some valid image extensions in array
+    let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
+
+    // If user selected file is an image file
+    if (validExtensions.includes(fileType)) {
+      let fileReader = new FileReader();
+      fileReader.onload = () => {
+        // Passing user file source in fileURL variable
+        let fileURL = fileReader.result;
+
+        // Creating an img tag and passing user selected file source inside src attr
+        let imgTag = `<img class="ctgr-logo" src="${fileURL}" alt="image" style="height: 90%; width: 90%; border-radius: 6px; object-fit: cover; border-radius: 5px;">`;
+
+        // Adding that created img tag inside dropArea container
+        this.dropArea.nativeElement.innerHTML = imgTag;
+      }
+      fileReader.readAsDataURL(this.file);
+    }
+    else {
+      alert("This is not an image file !");
+      this.dropArea.nativeElement.classList.remove("active");
+      this.dragText.nativeElement.textContent = "Drag & Drop";
+    }
+  }
 }
