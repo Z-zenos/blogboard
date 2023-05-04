33fa87d066284ec23d70dec05cf0f51ad0dba3c2
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2023-05-05 03:07:11 +0700

    [Create]: add emoji and image resize

--- src/app/forms/post-form/post-form.component.ts
+++ src/app/forms/post-form/post-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// import Emoji from 'quill-emoji'; -> Error, use below line
+// import "quill-emoji/dist/quill-emoji.css"; -> Error
 import 'quill-emoji/dist/quill-emoji.js';
 
 import Quill from 'quill';
@@ -12,16 +15,44 @@ Quill.register('modules/blotFormatter', BlotFormatter);
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.scss']
 })
+
 export class PostFormComponent implements OnInit {
+
+
+
+  // emojiIcon = '<svg class="i" viewBox="0 0 24 24"><use href="#emoticon-happy"></use></svg>';
 
   config = {
     'emoji-toolbar': true,
     'emoji-textarea': true,
     'emoji-shortname': true,
+
+    // "emoji-shortname": {
+    //   emojiList: emojiList,
+    //   fuse: {
+    //     shouldSort: true,
+    //     threshold: 0.1,
+    //     location: 0,
+    //     distance: 100,
+    //     maxPatternLength: 32,
+    //     minMatchCharLength: 1,
+    //     keys: [
+    //       "shortname"
+    //     ]
+    //   },
+    //   onOpen: function() { /* ... */ },
+    //   onClose: function(emojiListItem) { /* ... */ }
+    // },
+    // "emoji-toolbar": {
+    //   buttonIcon: this.emojiIcon
+    // },
+    // "emoji-textarea": {
+    //   buttonIcon: this.emojiIcon
+    // },
+
     toolbar: {
       container: [
         ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-        ['code-block'],
         // [{header: 1}, {header: 2}], // custom button values
         [{ list: 'ordered' }, { list: 'bullet' }],
         // [{'script': 'sub'}, {'script': 'super'}],      // superscript/subscript
@@ -29,7 +60,6 @@ export class PostFormComponent implements OnInit {
         [{ direction: 'rtl' }], // text direction
         [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
         [{ header: [1, 2, 3, 4, 5, 6, false] }],
-        ['image'],                                    // image
         ['code-block'], // code block
         [{ align: [] }],
         ['emoji'],
